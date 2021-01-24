79a6578d4e7eba77dd22539f7b6ac485443d7a01
Daniel Bowyer <danielbowyer@Daniels-MacBook-Pro.local>
2021-01-24 15:57:53 +0000

    added header component and links to shop plus home

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
+import Header from '../src/components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
